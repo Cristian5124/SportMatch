8a14e68f035a6fa1d926ba092f6e7bc222ffceec
Cristian <crispo5124@gmail.com>
2024-05-19 22:56:19 -0500

    Delete Tablas directory

--- Tablas/LlavesUnicas(UK).sql
+++
@@ -1,6 +0,0 @@
---Llaves Unicas (UK)
-
-ALTER TABLE ZonasDeportivas ADD CONSTRAINT UK_ZonasDeportivas UNIQUE(direccion);
-ALTER TABLE Encargados ADD CONSTRAINT UK_Encargados UNIQUE(foto);
-ALTER TABLE Jugadores ADD CONSTRAINT UK_Jugadores UNIQUE(correo);
-ALTER TABLE Equipos ADD CONSTRAINT UK_Equipos UNIQUE(nombre);
--- Tablas/Tuplas.sql
+++
@@ -1,3 +0,0 @@
---Tuplas
-
-ALTER TABLE Reservaciones ADD CONSTRAINT CK_Reservaciones_fechaSolicitud_fechaReservacion CHECK (fechaSolicitud <= fechaReservacion);
